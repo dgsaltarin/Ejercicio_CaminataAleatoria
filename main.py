a221c8f5897df84f77915dda41204743c8de9c76
dgsaltarin <dgsaltarin@gmail.com>
2022-01-02 21:14:27 -0500

    implemented random walk using two dimensions

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import random
-import matplotlib
 import matplotlib.pyplot as plt
 
-matplotlib.interactive(True)
 
-
-def random_walk():
+def random_walk(win_probability, lose_probability):
     max_position: int = 30
     min_position: int = 0
     time_point, position = 0, 20
@@ -17,7 +14,7 @@ def random_walk():
 
     while min_position < position < max_position:
         time_point += 1
-        movement = random.choice(movements)
+        movement = random.choices(movements, weights = [win_probability, lose_probability], k = 1)[0]
         if movement == "POSITIVE":
             position += 1
         if movement == "NEGATIVE":
@@ -33,8 +30,31 @@ def random_walk():
     return time_points, positions
 
 
+def random_walk_2d(observations: int):
+    x, y = 0, 0
+    positions = [y]
+    time_points = [x]
+    movements = ["UP", "DOWN", "RIGHT", "LEFT"]
+
+    for observations in range(1, observations + 1):
+        movement = random.choices(movements, weights = [25, 25, 25, 25], k = 1)[0]
+        if movement == "UP":
+            y += 1
+        if movement == "DOWN":
+            y -= 1
+        if movement == "RIGHT":
+            x += 1
+        if movement == "LEFT":
+            x -= 1
+
+        positions.append(y)
+        time_points.append(x)
+
+    return time_points, positions
+
+
 def run():
-    time_data, position_data = random_walk()
+    time_data, position_data = random_walk_2d(1000)
     plt.plot(time_data, position_data, 'r-')
     plt.title("1D Random Walk in Python")
     plt.show()
